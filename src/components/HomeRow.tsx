11805bedd96834e603d2175b14d2a745de88020c
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-09-03 11:41:17 +0000

    Create Home Components

---
+++ src/components/HomeRow.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HomeRow: React.FC = () => {
+  
+  return (
+    <div>HomeRow</div>
+  )
+}
+
+export default HomeRow;
---
+++ src/components/MealCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MealCard: React.FC = () => {
+  return (
+    <div>MealCard</div>
+  )
+}
+
+export default MealCard;
