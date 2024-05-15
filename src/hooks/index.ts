958559636d4b6c531a19fe2021c93abeff45ded4
Charles <okoyecharles509@gmail.com>
2024-05-15 15:36:20 +0100

    chore: delete old redux code

--- src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
-import { useNavigate, useNavigationType } from "react-router-dom";
-import { RootState } from "../components/Models";
-import store from "../redux/configureStore";
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
